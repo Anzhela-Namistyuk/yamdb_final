62d73475ac91ce9b98dde33e3fb81e48ed0b1dd3
Анжела <anzhela.namistyuk@yandex.ru>
2022-02-12 11:35:38 +0300

    15-th commit

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -5,10 +5,7 @@ import os
 from glob import glob
 
 from django.core.management.base import BaseCommand
-
-
-from reviews.models import (Category, Comment, Genre, GenreTitle,
-                            Review, Title)
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
 from users.models import User
 
 

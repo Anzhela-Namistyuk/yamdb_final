ee1ea1c7fc1a0a3a07172f23577707b70c19be74
Анжела <anzhela.namistyuk@yandex.ru>
2022-02-12 12:39:39 +0300

    18-th commit

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework import permissions
 from django.contrib.auth import get_user_model
+from rest_framework import permissions
 
 User = get_user_model()
 
@@ -57,6 +57,7 @@ class AdminOrReadOnlyPermission(permissions.BasePermission):
             return True
         if request.user.role == User.ADMIN or request.user.is_superuser:
             return True
+        return False
 
     def has_object_permission(self, request, view, obj):
         """Метод проверяет является ли пользователь
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+
 
 from .validator import validate_year
 
